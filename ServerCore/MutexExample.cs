9fb0db0b5afc23666663311d736f8ecf7df8f157
mbshin <mbshin@swm.ai>
2024-06-24 15:36:37 +0900

    feat: #5 feat-add-multi-thread
    close #5

---
+++ ServerCore/MutexExample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    /*
+    internal class MutexExample
+    {
+        static int _num = 0;
+        static Mutex _lock = new Mutex(); // 커널 동기화 객체
+
+        static void Thread_1()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                _lock.WaitOne();
+                _num++;
+                _lock.ReleaseMutex();
+            }
+        }
+
+        static void Thread_2()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                _lock.WaitOne();
+                _num--;
+                _lock.ReleaseMutex();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(_num);
+        }
+    }
+    */
+}
