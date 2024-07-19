9272c34a4fbfe7a3a4fe554f6d2ae9c7de4978d6
mbshin <mbshin@swm.ai>
2024-07-20 00:50:57 +0900

    feat: #15 feat-add-packet-generator

---
+++ PacketGenerator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Xml;
+
+namespace PacketGenerator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings()
+            {
+                IgnoreComments = true,
+                IgnoreWhitespace = true
+            };
+
+            using (XmlReader r = XmlReader.Create("PDL.xml", settings))
+            {
+                r.MoveToContent();
+
+                while (r.Read())
+                {
+                    if (r.Depth == 1 && r.NodeType == XmlNodeType.Element)
+                        ParsePacket(r);
+
+                    //Console.WriteLine($"{r.Name} {r["name"]}");
+                }
+            }
+        }
+
+        public static void ParsePacket(XmlReader r)
+        {
+            if (r.NodeType == XmlNodeType.EndElement)
+                return;
+
+            if (r.Name.ToLower() != "packet")
+            {
+                Console.WriteLine("Invalid packet node");
+                return;
+            }
+
+            string packetName = r["name"];
+            if (string.IsNullOrEmpty(packetName))
+            {
+                Console.WriteLine("Packet without name");
+                return;
+            }
+
+            ParseMembers(r);
+        }
+
+        public static void ParseMembers(XmlReader r)
+        {
+            string packetName = r["name"];
+
+            while (r.Read())
+            {
+
+            }
+        }
+    }
+}
