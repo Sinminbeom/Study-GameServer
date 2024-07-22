981db293654372dc169be99b1a09da556091fd3a
sinminbeom <asdf961008@gmail.com>
2024-07-23 00:58:46 +0900

    feat: #15 feat-add-packet-generator

---
+++ PacketGenerator/PacketFormat.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacketGenerator
+{
+    class PacketFormat
+    {
+        // {0} 패킷 이름
+        // {1} 멤버 변수들
+        // {2} 멤버 변수 Read
+        // {3} 멤버 변수 Write
+        public static string packetFormat =
+@"public class {0}
+{{
+    {1}
+
+    public override void Read(ArraySegment<byte> segment)
+    {{
+        ushort count = 0;
+        Span<byte> span = new Span<byte>(segment.Array, segment.Offset, segment.Count);
+
+        count += sizeof(ushort);
+        count += sizeof(ushort);
+
+        {2}
+    }}
+
+    public override ArraySegment<byte> Write()
+    {{
+        ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+        byte[] size = BitConverter.GetBytes(this.size);
+        byte[] packetId = BitConverter.GetBytes(this.packetId);
+        byte[] playerId = BitConverter.GetBytes(this.playerId);
+
+        ushort count = 0;
+        bool success = true;
+
+        Span<byte> span = new Span<byte>(segment.Array, segment.Offset, segment.Count);
+
+        count += sizeof(ushort);
+        success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), (ushort)PacketID.{0});
+        count += sizeof(ushort);
+        {3}
+        success &= BitConverter.TryWriteBytes(span, count);
+
+        if (success == false)
+            return null;
+
+        ArraySegment<byte> sendBuffer = SendBufferHelper.Close(count);
+        return sendBuffer;
+    }}
+}}";
+        // {0} 변수 형식
+        // {1} 변수 이름
+        public static string memberFormat =
+@"public {0} {1};";
+
+        public static string memberListFormat =
+@"";
+
+        // {0} 변수 이름
+        // {1} To- 변수 형식
+        // {2} 변수 형식
+        public static string readFormat =
+@"this.{0} = BitConverter.{1}(span.Slice(count, span.Length - count));
+count += sizeof({2});";
+        // {0} 변수 이름
+        // {1} To- 변수 형식
+        // {2} 변수 형식
+        public static string readStringFormat =
+@"ushort {0}Len = BitConverter.ToUInt16(span.Slice(count, span.Length - count));
+count += sizeof({2});
+this.{0} = Encoding.Unicode.GetString(span.Slice(count, {0}Len));
+count += {0}Len;";
+
+        // {0} 변수이름
+        // {1} 변수형식
+        public static string writeFormat =
+@"success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), this.{0});
+count += sizeof({1});";
+
+        // {0} 변수이름
+
+        public static string writeStringFormat =
+@"ushort nameLen = (ushort)Encoding.Unicode.GetBytes(this.{0}, 0, this.{0}.Length, segment.Array, segment.Offset + count + sizeof(ushort));
+success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), {0}Len);
+count += sizeof(ushort);
+count += {0}Len;
+";
+    }
+}
--- PacketGenerator/Program.cs
+++ PacketGenerator/Program.cs
@@ -2,8 +2,10 @@
 
 namespace PacketGenerator
 {
-    public class Program
+    class Program
     {
+        static string genPackets;
+
         private static void Main(string[] args)
         {
             XmlReaderSettings settings = new XmlReaderSettings()
@@ -21,8 +23,10 @@ namespace PacketGenerator
                     if (r.Depth == 1 && r.NodeType == XmlNodeType.Element)
                         ParsePacket(r);
 
-                    Console.WriteLine($"{r.Name} {r["name"]}");
+                    //Console.WriteLine($"{r.Name} || {r["name"]}");
                 }
+
+                File.WriteAllText("GenPackets.cs", genPackets);
             }
         }
 
@@ -44,12 +48,20 @@ namespace PacketGenerator
                 return;
             }
 
-            ParseMembers(r);
+            Tuple<string, string, string> t = ParseMembers(r);
+            genPackets += string.Format(PacketFormat.packetFormat, packetName, t.Item1, t.Item2, t.Item3);
         }
 
-        public static void ParseMembers(XmlReader r)
+        // {1} 멤버 변수들
+        // {2} 멤버 변수 Read
+        // {3} 멤버 변수 Write
+        public static Tuple<string, string, string> ParseMembers(XmlReader r)
         {
             string packetName = r["name"];
+
+            string memberCode = "";
+            string readCode = "";
+            string writeCode = "";
 
             int depth = r.Depth + 1;
             while (r.Read())
@@ -61,11 +73,72 @@ namespace PacketGenerator
                 if (string.IsNullOrEmpty(memberName))
                 {
                     Console.WriteLine("Member without name");
-                    return;
+                    return null;
                 }
 
-                string memberType = r.Name.ToLower();
+                if (string.IsNullOrEmpty(memberCode) == false)
+                    memberCode += Environment.NewLine;
 
+                if (string.IsNullOrEmpty(readCode) == false)
+                    readCode += Environment.NewLine;
+
+                if (string.IsNullOrEmpty(writeCode) == false)
+                    writeCode += Environment.NewLine;
+
+                string memberType = r.Name.ToLower();
+                switch (memberType)
+                {
+                    case "bool":
+                    case "byte":
+                    case "short":
+                    case "ushort":
+                    case "int":
+                    case "long":
+                    case "float":
+                    case "double":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readFormat, memberName, ToMemberType(memberType), memberType);
+                        writeCode += string.Format(PacketFormat.writeFormat, memberName, memberType);
+                        break;
+                    case "string":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readStringFormat, memberName, ToMemberType(memberType), memberType);
+                        writeCode += string.Format(PacketFormat.writeStringFormat, memberName, memberType);
+                        break;
+                    case "list":
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            memberCode = memberCode.Replace("\n", "\n\t");
+            readCode = readCode.Replace("\n", "\n\t\t");
+            writeCode = writeCode.Replace("\n", "\n\t\t");
+            return new Tuple<string, string, string>(memberCode, readCode, writeCode);
+        }
+
+        public static string ToMemberType(string memberType)
+        {
+            switch (memberType)
+            {
+                case "bool":
+                    return "ToBoolean";
+                case "short":
+                    return "ToInt16";
+                case "ushort":
+                    return "ToUInt16";
+                case "int":
+                    return "ToInt32";
+                case "long":
+                    return "ToInt64";
+                case "float":
+                    return "ToSingle";
+                case "double":
+                    return "ToDouble";
+                default:
+                    return "";
             }
         }
     }
