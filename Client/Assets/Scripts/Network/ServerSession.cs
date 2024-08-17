25f90d8e7507c075e64ae93f0ec2cb4851a8f0ae
sinminbeom <asdf961008@gmail.com>
2024-08-17 23:28:19 +0900

    feat: #23 add-link-unity
    close #23

---
+++ Client/Assets/Scripts/Network/ServerSession.cs
@@ -0,0 +1,45 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyClient
+{
+    class ServerSession : PacketSession
+    {
+        //static unsafe void ToBytes(byte[] array, int offset, ulong value)
+        //{
+        //    fixed (byte* ptr = &array[offset])
+        //        *(ulong*)ptr = value;
+        //}
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected {endPoint}");
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected {endPoint}");
+        }
+
+        //public override int OnRecv(ArraySegment<byte> buffer)
+        //{
+        //    string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+        //    Console.WriteLine($"[From Server] {recvData}");
+        //    return buffer.Count;
+        //}
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            PacketManager.Instance.OnRecvPacket(this, buffer);
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            //Console.WriteLine($"Send Transferred bytes: {numOfBytes}");
+        }
+    }
+}
---
+++ Client/Assets/Scripts/Packet/PacketHandler.cs
@@ -0,0 +1,32 @@
+﻿using DummyClient;
+using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+// 해당 패킷이 다 조립이 되었으면 무엇을 호출할지를 정의한다.
+class PacketHandler
+{
+        
+    public static void S_ChatHandler(PacketSession session, IPacket packet)
+    {
+        S_Chat chatPacket = packet as S_Chat;
+        ServerSession serverSession = session as ServerSession;
+        if (chatPacket.playerId == 1)
+        {
+            Debug.Log(chatPacket.chat);
+            GameObject go = new GameObject();
+            if (go == null)
+                Debug.Log("Player not found");
+            else
+                Debug.Log("Player found");
+        }
+        //Console.WriteLine(chatPacket.chat);
+        //if (chatPacket.playerId == 1)
+        //    Console.WriteLine(chatPacket.chat);
+
+    }
+}
